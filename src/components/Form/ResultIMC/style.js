f8d1b9fd6928a0caa9ef5678f0a9c695df1ff2ff
daniellucena1 <daniellucena.jt@gmail.com>
2024-03-28 00:15:16 -0300

    adding share button

--- src/components/Form/ResultIMC/style.js
+++ src/components/Form/ResultIMC/style.js
@@ -18,6 +18,22 @@ const styles = StyleSheet.create({
         fontSize: 48,
         color: "#ff0043",
         fontWeight: "bold",
+    },
+    boxShareButton: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    share: {
+        backgroundColor: "#1877f2",
+        borderRadius: 50,
+        paddingBottom: 5,
+        paddingTop: 5,
+    },
+    textShare: {
+        color: "#ffffff",
+        fontWeight: "bold",
+        paddingHorizontal: 30,
     }
 
 });
